9db2dfcb88a7c2b530f295e9c03e01ddd0c69bd6
enaikele omoh kelvin <enaikeleomoh@gmail.com>
2023-08-24 10:57:37 +0100

    updated

--- src/Firebase.js
+++ src/Firebase.js
@@ -17,6 +17,5 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
-const analytics = getAnalytics(app);
 export const auth = getAuth(app);
 export default app
